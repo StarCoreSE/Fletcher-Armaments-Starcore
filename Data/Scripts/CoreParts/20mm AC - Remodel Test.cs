5b47743a1722c5aaaeb78c643c77b9db4fbe89bb
TaitKaito <clayton.h.c123@gmail.com>
2023-03-30 03:19:12 +0100

    Initial Upload

---
+++ Data/Scripts/CoreParts/20mm AC - Remodel Test.cs
@@ -0,0 +1,162 @@
+﻿using static Scripts.Structure;
+using static Scripts.Structure.WeaponDefinition;
+using static Scripts.Structure.WeaponDefinition.ModelAssignmentsDef;
+using static Scripts.Structure.WeaponDefinition.HardPointDef;
+using static Scripts.Structure.WeaponDefinition.HardPointDef.Prediction;
+using static Scripts.Structure.WeaponDefinition.TargetingDef.BlockTypes;
+using static Scripts.Structure.WeaponDefinition.TargetingDef.Threat;
+using static Scripts.Structure.WeaponDefinition.HardPointDef.HardwareDef;
+using static Scripts.Structure.WeaponDefinition.HardPointDef.HardwareDef.HardwareType;
+
+namespace Scripts {   
+    partial class Parts {
+        // Don't edit above this line
+        WeaponDefinition FA20MmACRemodel => new WeaponDefinition {
+
+            Assignments = new ModelAssignmentsDef 
+            {
+                MountPoints = new[] {
+                    new MountPointDef {
+                        SubtypeId = "20mmGunSG",
+                        SpinPartId = "None", // For weapons with a spinning barrel such as Gatling Guns.
+                        MuzzlePartId = "Barrel",
+                        AzimuthPartId = "None",
+                        ElevationPartId = "None",
+                    },
+
+                },
+                Muzzles = new[] {
+                    "muzzle_missile_1",
+					
+
+                },
+            },
+            Targeting = new TargetingDef  
+            {
+                Threats = new[] {
+                    Grids, Projectiles, Meteors, Characters,
+                },
+                SubSystems = new[] {
+                    Thrust, Utility, Offense, Power, Production, Any,
+                },
+                ClosestFirst = false, // tries to pick closest targets first (blocks on grids, projectiles, etc...).
+                IgnoreDumbProjectiles = false, // Don't fire at non-smart projectiles.
+                LockedSmartOnly = false, // Only fire at smart projectiles that are locked on to parent grid.				
+                MinimumDiameter = 0, // 0 = unlimited, Minimum radius of threat to engage.
+                MaximumDiameter = 0, // 0 = unlimited, Maximum radius of threat to engage.
+                MaxTargetDistance = 1200, // 0 = unlimited, Maximum target distance that targets will be automatically shot at.
+                MinTargetDistance = 0, // 0 = unlimited, Min target distance that targets will be automatically shot at.				
+                TopTargets = 8, // 0 = unlimited, max number of top targets to randomize between.
+                TopBlocks = 4, // 0 = unlimited, max number of blocks to randomize between
+                StopTrackingSpeed = 1500, // do not track target threats traveling faster than this speed
+            },
+            HardPoint = new HardPointDef 
+            {
+                PartName = "20mm Aircraft Cannon", // name of weapon in terminal
+                DeviateShotAngle = 0.1f,
+                AimingTolerance = 4.15f, // 0 - 180 firing angle
+                AimLeadingPrediction = Accurate, // Off, Basic, Accurate, Advanced
+                DelayCeaseFire = 30, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+
+                Ui = new UiDef {
+                    RateOfFire = true,
+                    DamageModifier = false,
+                    ToggleGuidance = false,
+                    EnableOverload =  false,
+                },
+                Ai = new AiDef {
+                    TrackTargets = false,
+                    TurretAttached = false,
+                    TurretController = false,
+                    PrimaryTracking = false,
+                    LockOnFocus = false,
+                },
+                HardWare = new HardwareDef {
+                    RotateRate = 0.0f,
+                    ElevateRate = 0.0f,
+                    MinAzimuth = 0,
+                    MaxAzimuth = 0,
+                    MinElevation = 0,
+                    MaxElevation = 0,
+                    FixedOffset = false,
+                    InventorySize = 0.6f,
+                    Offset = Vector(x: 0, y: 0, z: 0),
+                    Type = BlockWeapon, // What type of weapon this is; BlockWeapon, HandWeapon, Phantom 
+                },
+                Other = new OtherDef {
+                    ConstructPartCap = 0, // Maximum number of blocks with this weapon on a grid; 0 = unlimited.
+                    RotateBarrelAxis = 0,
+                    EnergyPriority = 0,
+                    MuzzleCheck = false,
+                    Debug = false,
+                    RestrictionRadius = 0f, // Meters, radius of sphere disable this gun if another is present
+                    CheckInflatedBox = false, // if true, the bounding box of the gun is expanded by the RestrictionRadius
+                    CheckForAnyWeapon = false, // if true, the check will fail if ANY gun is present, false only looks for this subtype					
+                },
+                Loading = new LoadingDef {
+                    RateOfFire = 350,
+                    BarrelSpinRate = 0, // visual only, 0 disables and uses RateOfFire
+                    BarrelsPerShot = 1,
+                    TrajectilesPerBarrel = 1, // Number of Trajectiles per barrel per fire event.
+                    SkipBarrels = 0,
+                    ReloadTime = 120, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+					MagsToLoad = 2, // Number of physical magazines to consume on reload.
+                    DelayUntilFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    HeatPerShot = 0, //heat generated per shot
+                    MaxHeat = 70000, //max heat before weapon enters cooldown (70% of max heat)
+                    Cooldown = .50f, //percent of max heat to be under to start firing again after overheat accepts .2-.95
+                    HeatSinkRate = 6800, //amount of heat lost per second
+                    DegradeRof = false, // progressively lower rate of fire after 80% heat threshold (80% of max heat)
+                    ShotsInBurst = 0,
+                    DelayAfterBurst = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    FireFull = false,
+                },
+                Audio = new HardPointAudioDef {
+                    PreFiringSound = "",
+                    FiringSound = "20mmACFire", // WepShipGatlingShot
+                    FiringSoundPerShot = true,
+                    ReloadSound = "",
+                    NoAmmoSound = "",
+                    HardPointRotationSound = "",
+                    BarrelRotationSound = "",
+					FireSoundEndDelay = 120, // Measured in game ticks(6 = 100ms, 60 = 1 seconds, etc..)	                    
+                },
+                Graphics = new HardPointParticleDef
+                {
+                    Effect1 = new ParticleDef {
+                        Name = "Muzzle_Flash_Autocannon", // Smoke_LargeGunShot
+                        Color = Color(red: 15, green: 2, blue: 1, alpha: 0.8f),
+                        Offset = Vector(x: 0, y: 0, z: 0),
+
+                        Extras = new ParticleOptionDef {
+                            Loop = false,
+                            Restart = false,
+                            MaxDistance = 300,
+                            MaxDuration = 0f,
+                            Scale = 1f,
+                        },
+                    },
+                    Effect2 = new ParticleDef {
+                        Name = "Smoke_Autocannon",//Muzzle_Flash_Large
+                        Color = Color(red: 1, green: 1, blue: 1, alpha: 1f),
+                        Offset = Vector(x: 0, y: 0, z: 0),
+
+                        Extras = new ParticleOptionDef {
+                            Loop = true,
+                            Restart = false,
+                            MaxDistance = 350,
+                            MaxDuration = 6f,
+                            Scale = 1.0f,
+                        },
+                    },
+                },
+            },
+            Ammos = new [] {
+               FA20mmACMag
+
+            },
+            Animations = AC20mmAnimation,
+            // Don't edit below this line
+        };
+    }
+}
---
+++ Data/Scripts/CoreParts/MasterConfig.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            // naming convention: WeaponDefinition Name
+            //
+            // Enable your definitions using the follow syntax:
+            // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
+            // PartDefinitions includes both weapons and phantoms
+            PartDefinitions(
+			FA127mmSingle,
+			FA127mmDual,
+			FA127mmMk24,
+			FA12mmmMk12,
+			FA127mmMK56,
+			FA128mmL45,
+			FA128mmC34,
+			FA15CmTwin,
+			FA15CmSingle,
+			FA138mmMLE,
+			FA15CmCATwin,
+			FA15CmCATwinNR,
+			FA152mmMLE,
+			FA152mmMLER,
+			FA152mmMLE1936,
+			FA203mmMLE1931,
+			FA203mmMLE1931NR,
+			FA6InchTripleCa,
+			FA20MmAC,
+			FA12MG,
+			FA381mmR,
+			FA381mmAlt,
+			FA16InchGun,
+			FA380mmMLE,
+			
+			FA20MmACRemodel,
+			FaOerlikon20mmRemodel,
+			FACaseMateTest,
+			FaOerlikon20mmDualRemodel,
+			FA16InchGunRemodel,
+			FaFlak2cm38Remodel,
+			FaFlak37mmRemodel,
+			FaBoforSingleRemodel,
+			FaBoforTwinRemodel,
+			FA127mmDualRemodel,
+			FaBoforQuadRemodel,
+			FA127mmSingleRemodel,
+			FaHotch13mmRemodel,
+			FaHotch13mmQuadRemodel,
+			FaPomPomRemodel,
+			FaMLE37mmRemodel,
+			FaACAD37mmRemodel,
+			FaFlak38Remodel,
+			FaTwin105mmRemodel,
+			FaSingle100MMRemodel,
+			FaTwin100MMRemodel,
+			FaTwin100MM1945Remodel,
+			FA127mmMk30Remodel,
+			FA6Inch47Remodel,
+			FA5Inch54,
+			FA8Inch55,
+			FA128mmC34Remodel,
+			FA128mmL45Remodel,
+			FA138mmMLERemodel,
+			FAHvarRemodelWC,
+			FA120mmMk12Remodel,
+			FaOerlikon20mmSmallRemodel,
+			FaOerlikon20mmDualSmallRemodel,
+			FaFlak2cm38RemodelSmall,
+			FaBoforSingleSmallRemodel,
+			FaFlak38SmallRemodel,
+			FaFlak37mmSmallRemodel,
+            SingleCasemate
+
+            );
+          
+        }
+    }
+}
