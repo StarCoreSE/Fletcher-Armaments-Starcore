9c1f09ccef7f4ffcd880d926cee11e0894277170
TaitKaito <clayton.h.c123@gmail.com>
2023-06-04 00:31:19 +0100

    see mod-changelogs

--- Data/Scripts/CoreParts/127mm Mark 56.cs
+++ Data/Scripts/CoreParts/127mm Mark 56.cs
@@ -19,17 +19,17 @@ namespace Scripts {
                     new MountPointDef {
                         SubtypeId = "127mmMk56",  // Block Subtypeid. Your Cubeblocks contain this information
                         SpinPartId = "",   // For weapons with a spinning barrel such as Gatling Guns. Subpart_Boomsticks must be written as Boomsticks.
-                        MuzzlePartId = "MissileTurretBarrels",  // The subpart where your muzzle empties are located. This is often the elevation subpart. Subpart_Boomsticks must be written as Boomsticks.
-                        AzimuthPartId = "MissileTurretBase1",  // Your Rotating Subpart, the bit that moves sideways.
-                        ElevationPartId = "MissileTurretBarrels",  // Your Elevating Subpart, that bit that moves up.
+                        MuzzlePartId = "elevation",  // The subpart where your muzzle empties are located. This is often the elevation subpart. Subpart_Boomsticks must be written as Boomsticks.
+                        AzimuthPartId = "azimuth",  // Your Rotating Subpart, the bit that moves sideways.
+                        ElevationPartId = "elevation",  // Your Elevating Subpart, that bit that moves up.
                         DurabilityMod = 0.25f, // GeneralDamageMultiplier, 0.25f = 25% damage taken.
                         IconName = "TestIcon.dds" // Overlay for block inventory slots, like reactors, refineries, etc.
                     },
                     
                  },
                 Muzzles = new[] {
-                    "muzzle_missile_01",
-					"muzzle_missile_02",
+                    "barrel_1",
+					"barrel_2",
                     				
                     
                 },
