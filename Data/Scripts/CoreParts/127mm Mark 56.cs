c846f13351a42a1ec7116224335a62f5bd103f6f
TaitKaito <clayton.h.c123@gmail.com>
2023-06-20 17:41:39 +0100

    thanks dankstnak

--- Data/Scripts/CoreParts/127mm Mark 56.cs
+++ Data/Scripts/CoreParts/127mm Mark 56.cs
@@ -80,7 +80,7 @@ namespace Scripts {
                     PrimaryTracking = true, // For multiweapons: whether this weapon should designate targets for other weapons on the platform without their own tracking.
                     LockOnFocus = false, // If enabled, weapon will only fire at targets that have been HUD selected AND locked onto by pressing Numpad 0.
                     SuppressFire = false, // If enabled, weapon can only be fired manually.
-                    OverrideLeads = true, // Disable target leading on fixed weapons, or allow it for turrets.
+                    OverrideLeads = false, // Disable target leading on fixed weapons, or allow it for turrets.
                 },
                 HardWare = new HardwareDef
                 {
